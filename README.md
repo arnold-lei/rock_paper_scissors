6e4a61b1ba0e5f5995d01267dd0daa7a325ba924
Arnold Lei <arnold.lei88@gmail.com>
2014-09-29 23:50:40 -0400

    Updated the README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+READ ME
+
+Hi All,
+
+Welcome to my rock paper scissors application. I had a lot of fun creating this game. I don't think I did it the best way possible but it is all origina work. I see that the solution's code is far more elegant than the way I've done it. Any feedback would be greatly appreciated.
--- game.rb
+++ game.rb
@@ -6,31 +6,51 @@ ans = 'y'
 
 def game(choice,cpu)
 	if choice == 'r' && cpu == 2
-		say "You picked Rock and the Computer chose Paper you Lose."
+		say "You picked Rock" 
+		say "and the Computer chose Paper" 
+		say"You Lose."
 	elsif choice == 'r' && cpu == 3
-		say "You picked Rock and the Computer chose Scissors you Win!"
+		say "You picked Rock" 
+		say "and the Computer chose Scissors" 
+		say "You Win!"
 	elsif choice == 'r' && cpu == 1
-		say "You picked Rock and the Computer chose Rock you Tie!"
+		say "You picked Rock" 
+		say "and the Computer chose Rock" 
+		say "You Tie!"
 	elsif choice == 's' && cpu == 2
-		say "You picked Scissors and the Computer chose Paper you Win!"
+		say "You picked Scissors" 
+		say "and the Computer chose Paper" 
+		say "You Win!"
 	elsif choice == 's' && cpu == 3
-		say "You picked Scissors and the Computer chose Scissors you Tie!"
+		say "You picked Scissors" 
+		say "and the Computer chose Scissors" 
+		say "You Tie!"
 	elsif choice == 's' && cpu == 1
-		say "You picked Scissors and the Computer chose Rock you Lose!"
+		say "You picked Scissors" 
+		say "and the Computer chose Rock" 
+		say "You Lose!"
 	elsif choice == 'p' && cpu == 2
-		say "You picked Paper and the Computer chose Paper you Tie!"
+		say "You picked Paper" 
+		say "and the Computer chose Paper" 
+		say "You Tie!"
 	elsif choice == 'p' && cpu == 3
-		say "You picked Paper and the Computer chose Scissors you Lose!"
+		say "You picked Paper" 
+		say "and the Computer chose Scissors" 
+		say "You Lose!"
 	elsif choice == 'p' && cpu == 1
-		say "You picked Paper and the Computer chose Rock you Win!"
+		say "You picked Paper" 
+		say "and the Computer chose Rock" 
+		say "You Win!"
+	else
+		say "Please pick again you may only pick r, p, or s"
 	end
 end
 
-while ans == 'y' do 
+while ans == 'y'  
 	prng = Random.new 
 	say "Let's play rock paper scissors!"
 	say "Play one: (R/P/S)"
-	choice = gets.chomp
+	choice = gets.chomp.downcase
 	cpu = prng.rand(1..3)
 	game(choice,cpu)
 	say "Do you want to play again? Y/N"
